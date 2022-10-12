004b5583bfc95a2ca83befd3dd2b45ec60bcc2f0
Louie Moretti <morettilouie@gmail.com>
2022-10-12 04:59:38 -0500

    added InjectManifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,6 +21,10 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Text Editor'
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js'
       })
     ],
 
